62453329327543391cc4cdf9b43fc41fc23cc90f
Mutsuha Asada <momiimt@gmail.com>
2022-10-16 09:20:50 +0000

    🛠 fix: generates directories

--- generator/src/generator.nim
+++ generator/src/generator.nim
@@ -13,31 +13,35 @@ const Library = Base & Details
 registerLibrary(Library)
 
 var pages: seq[tuple[text, href: string]] = @[]
-for dir in walkDir("../articles"):
-  if dir.kind != pcDir:
-    raise newException(Exception, "ファイルが存在してはいけない")
-
-  let name = dir.path.split('/')[^1][10..^1]
-  echo name
-  block:
-    var outputFile = open(&"../dist/{name}.html", FileMode.fmWrite)
-    defer: outputFile.close()
-    outputFile.write(
-      generateArticleHtml(
-        lex(dir.path & "/index.[]").parse().generate()
-      )
-    )
-  let title = block:
-    let
-      stdlib = "/nim/lib"
-      intr = createInterpreter(dir.path & "/" & "setting.nims", [stdlib, stdlib / "pure", stdlib / "core"])
-    intr.evalScript()
-    defer: intr.destroyInterpreter()
-    let sym = intr.selectUniqueSymbol("title")
-    let val = intr.getGlobalValue(sym).strVal
-    val
-  
-  pages.add (title, name & ".html")
+for yearDir in walkDir("../articles"):
+  let year = yearDir.path.split('/')[^1]
+  for monthDir in walkDir(yearDir.path):
+    let month = monthDir.path.split('/')[^1]
+    for dayDir in walkDir(monthDir.path):
+      let day = dayDir.path.split('/')[^1]
+      for dir in walkDir(dayDir.path):
+        let name = dir.path.split('/')[^1]
+        echo name
+        block:
+          createDir(&"../dist/{year}/{month}/{day}/")
+          var outputFile = open(&"../dist/{year}/{month}/{day}/{name}.html", FileMode.fmWrite)
+          defer: outputFile.close()
+          outputFile.write(
+            generateArticleHtml(
+              lex(dir.path & "/index.[]").parse().generate()
+            )
+          )
+        let title = block:
+          let
+            stdlib = "/nim/lib"
+            intr = createInterpreter(dir.path & "/" & "setting.nims", [stdlib, stdlib / "pure", stdlib / "core"])
+          intr.evalScript()
+          defer: intr.destroyInterpreter()
+          let sym = intr.selectUniqueSymbol("title")
+          let val = intr.getGlobalValue(sym).strVal
+          val
+        
+        pages.add (title, &"{year}/{month}/{day}/{name}.html")
 
 block:
   var outputFile = open(&"../dist/index.html", FileMode.fmWrite)
