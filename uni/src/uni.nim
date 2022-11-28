fe8c28ae6cf975f287df5f4a2a6e3df68ecb8bff
momeemt <momiimt@gmail.com>
2022-11-28 19:13:24 +0900

    👍 add: hotreload server

--- uni/src/uni.nim
+++ uni/src/uni.nim
@@ -3,9 +3,15 @@ import std/times
 import std/strformat
 import std/strutils
 import std/random
+import std/asyncdispatch
+import std/asynchttpserver
+import std/json
+import nwatchdog
 
-include "templates/article.settings.toml.nimf"
-include "templates/daily.settings.toml.nimf"
+import uni/builder
+
+include "scfs/article.settings.toml.nimf"
+include "scfs/daily.settings.toml.nimf"
 
 proc today (): string =
   let now = now()
@@ -41,9 +47,58 @@ proc newDaily (date: string = today()): int =
     setting.write(dailySettingsToml(rand(1..16)))
     setting.close()
 
+proc preview () =
+  var clients: seq[Request] = @[]
+  proc serve {.async.} =
+    var server = newAsyncHttpServer()
+    proc cb(req: Request) {.async.} =
+      if req.url.path == "/poll":
+        clients.add req
+        return
+      let headers = case $req.url.path.split('.')[^1]
+                    of "html": {"Content-type": "text/html; charset=utf-8"}
+                    of "css": {"Content-type": "text/css; charset=utf-8"}
+                    else: {"Content-type": "text/plain; charset=utf-8"}
+      let html = block:
+        let f = open("../dist" & req.url.path)
+        defer: f.close()
+        f.readAll
+      await req.respond(Http200, html, headers.newHttpHeaders())
+
+    server.listen(Port(3000))
+    let port = server.getPort
+    echo "test this with: curl localhost:" & $port.uint16 & "/"
+    while true:
+      if server.shouldAcceptRequest():
+        await server.acceptRequest(cb)
+      else:
+        await sleepAsync(500)
+
+  let wd = NWatchDog[string](interval: 100)
+  proc callback (file: string, evt: NWatchEvent, param: string) {.gcsafe async.} =
+    build()
+    for client in clients:
+      let headers = {"Content-type": "application/json; charset=utf-8"}
+      await client.respond(Http200, $(%*{ "status": "ok" }), headers.newHttpHeaders())
+
+  wd.add(
+    "../dailies",
+    "[\\w\\W]*\\.(\\[\\]|toml)",
+    callback,
+    "transpiled brack"
+  )
+
+  waitFor serve() and wd.watch
+
+proc buildBlog (): int =
+  builder.build()
+
 when isMainModule:
   import cligen
+
   dispatchMulti(
     [newArticle, cmdName = "new:article"],
     [newDaily, cmdName = "new:daily"],
+    [preview],
+    [buildBlog, cmdName = "build"]
   )
