26133e8f4b826a6bfd4105d2b5885b14fb8a4841
Mutsuha Asada <me@momee.mt>
2024-10-22 19:03:52 +0900

    init: blog 2024

--- README.md
+++
@@ -1,9 +0,0 @@
-# [blog.momee.mt](https://blog.momee.mt)
-
-[![Deploy](https://github.com/momeemt/blog.momee.mt/actions/workflows/deploy.yml/badge.svg)](https://github.com/momeemt/blog.momee.mt/actions/workflows/deploy.yml)
-
-momeemt's blog
-
-# System chart
-
-![](/drawio/blog.drawio.svg)
--- flake.nix
+++ flake.nix
@@ -1,30 +1,34 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    brack.url = "github:brack-lang/brack";
   };
-  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    flake-compat,
+    brack,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
         };
+        brack-bin = brack.packages.${system}.default;
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nim-unwrapped
-            nimPackages.nimble
-          ] ++ lib.optional stdenv.isDarwin (with pkgs.darwin; [
-            Security
-          ]);
-
-          shellHook = ''
-            export PATH=$PATH:$HOME/.nimble/bin
-          '';
+            nil
+            alejandra
+            brack-bin
+          ];
         };
       }
     );
